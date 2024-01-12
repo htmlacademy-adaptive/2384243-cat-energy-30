80f50355ab7aa3542b32f249cf9741dee031721c
Nargiza <nargizyonysh@gmail.com>
2024-01-12 11:53:25 +0500

    Добавляет декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nhttps://eslint.org/docs/latest/rules/prefer-arrow-callbackav--closed')) {
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+  } else {
+    navMain.classList.add('main-nav--opened');
+    navMain.classList.remove('main-nav--closed');
+  }
+});
