93333926e4a31513935a7399203801252baad051
Nargiza <nargizyonysh@gmail.com>
2024-01-16 13:45:54 +0500

    Вносит правки по декоративным элементам

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,13 +4,14 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nhttps://eslint.org/docs/latest/rules/prefer-arrow-callbackav--closed')) {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
-  } else {
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
   }
 });
